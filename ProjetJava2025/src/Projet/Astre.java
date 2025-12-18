d004edb7211b21fb77748d25adced8360155bb24
Lubin <Lubin@192.168.1.12>
2025-12-18 16:53:16 +0100

    Modifrication de la classe vecteur pour plus de flexibilité et classe astre et doublet ( au final on est obligé)Alexis verifie surtout la gestion des forces si ca te va

---
+++ ProjetJava2025/src/Projet/Astre.java
@@ -0,0 +1,110 @@
+package Projet;
+
+import java.util.ArrayList;
+
+public abstract class Astre {
+
+    /* ===== Attributs ===== */
+    private double rayon;
+    private double masse;
+
+    private ArrayList<Vecteur> forces;
+    private int nbPoints;
+
+    // Position et vitesse de l'astre à un instant t
+    private Doublet<Vecteur, Vecteur> etat;
+
+    // Trajectoire
+    private ArrayList<Integer> trajectoireX;
+    private ArrayList<Integer> trajectoireY;
+
+    //Constructeur
+    public Astre(double masse, double rayon, Vecteur positionInitiale, Vecteur vitesseInitiale) {
+                
+        this.masse = masse;
+        this.rayon = rayon;
+        this.forces = new ArrayList<>();
+        this.nbPoints = 0;
+
+        this.etat = new Doublet<>(positionInitiale, vitesseInitiale);
+
+        this.trajectoireX = new ArrayList<>();
+        this.trajectoireY = new ArrayList<>();
+
+        // Position initiale
+        trajectoireX.add((int) positionInitiale.getX());
+        trajectoireY.add((int) positionInitiale.getY());
+    }
+
+    //Trajectoire
+    public ArrayList<Integer> trajectoireX() {
+        return trajectoireX;
+    }
+
+    public ArrayList<Integer> trajectoireY() {
+        return trajectoireY;
+    }
+
+    protected void ajouterPointTrajectoire(Vecteur position) {
+        trajectoireX.add((int) position.getX());
+        trajectoireY.add((int) position.getY());
+    }
+
+    //gestion des Forces
+    public void ajouterForce(Vecteur f) {
+        forces.add(f);
+    }
+
+    protected Vecteur sommeForces() {
+        Vecteur somme = new Vecteur(0, 0, 0);
+        for (Vecteur f : forces) {
+            somme = somme.add(f);
+        }
+        return somme;
+    }
+
+    //methode d'Euler pour approximer les positions
+    public void update(double dt) {
+
+        Vecteur position = etat.getPremier();
+        Vecteur vitesse  = etat.getSecond();
+
+        // a = F / m
+        Vecteur acceleration = sommeForces().multiply(1.0 / masse);
+
+        // v(t+dt)
+        vitesse = vitesse.add(acceleration.multiply(dt));
+
+        // x(t+dt)
+        position = position.add(vitesse.multiply(dt));
+
+        // Mise à jour de l'état
+        etat = new Doublet<>(position, vitesse);
+
+        // Sauvegarde de la trajectoire
+        ajouterPointTrajectoire(position);
+
+        nbPoints++;
+        forces.clear();
+    }
+
+    //Methodes Abstrates
+    public abstract void affiche();
+
+    //getters
+    public double getRayon() {
+        return rayon;
+    }
+
+    public double getMasse() {
+        return masse;
+    }
+
+    public int getNbPoints() {
+        return nbPoints;
+    }
+
+    public Doublet<Vecteur, Vecteur> getEtat() {
+        return etat;
+    }
+}
--- ProjetJava2025/src/Projet/Vecteur.java
+++ ProjetJava2025/src/Projet/Vecteur.java
@@ -1,27 +1,75 @@
 package Projet;
 
 public class Vecteur {
-    private double angle;
-    private double norme;
-    private Point pointApplication;
 
-    public Vecteur(Point p, double norme, double angle) {
-        this.pointApplication = p;
-        this.angle = angle;
-        this.norme = norme;
-    }
+	private double x;
+	private double y;
+	private double z;
+	private double angle;
+	private double norme;
 
-    public Vecteur(Point p, Point q) {
-        this.pointApplication = p;
 
-        double dx = q.getX() - p.getX();
-        double dy = q.getY() - p.getY();
-        double dz = q.getZ() - p.getZ();
 
-        // Norme du vecteur
-        this.norme = Math.sqrt(dx * dx + dy * dy + dz * dz);
+	private Point pointApplication;
 
-        // Angle avec l'horizontale (axe x)
-        this.angle = Math.atan2(dy, dx);
-    }
+	//Vecteurs avec des coords
+	public Vecteur(double x, double y, double z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+		this.norme = Math.sqrt(x * x + y * y + z * z);
+		this.angle =  Math.atan2(y, x);
+	}
+
+	//Vecteur avec 2 points
+	public Vecteur(Point p, Point q) {
+		this.pointApplication = p;
+		this.x = q.getX() - p.getX();
+		this.y = q.getY() - p.getY();
+		this.z = q.getZ() - p.getZ();
+		this.norme = Math.sqrt(x * x + y * y + z * z);
+		this.angle =  Math.atan2(y, x);
+	}
+	//vecteur avec angle
+	public Vecteur(Point p, double norme, double angle) {
+		this.pointApplication = p;
+		this.angle = angle; 
+		this.norme = norme;
+		this.x = norme * Math.cos(angle);
+		this.y = norme * Math.sin(angle);
+		this.z = 0;
+
+	}
+
+	//Getters et Setters
+	public double getX() {
+		return x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public double getZ() {
+		return z;
+	}
+
+
+	public double getNorme() {
+		return this.norme;
+	}
+
+	/* ===== Angle (dans le plan XY) ===== */
+	public double getAngle() {
+		return this.angle;
+	}
+
+
+	public Vecteur add(Vecteur v) {
+		return new Vecteur(this.x + v.x, this.y + v.y, this.z + v.z);
+	}
+
+	public Vecteur multiply(double k) {
+		return new Vecteur(k * x, k * y, k * z);
+	}
 }
