19e085f8114d5fb9dad15dc7b12670c7a1790396
Lubin <Lubin@192.168.1.12>
2026-01-02 18:06:17 +0100

    changement classe point ( les setters etaient encore en int plutot que double)

--- ProjetJava2025/src/Projet/Point.java
+++ ProjetJava2025/src/Projet/Point.java
@@ -17,19 +17,19 @@ public class Point {
 	public double getX() {
 		return x;
 	}
-	public void setX(int x) {
+	public void setX(double x) {
 		this.x = x;
 	}
 	public double getY() {
 		return y;
 	}
-	public void setY(int y) {
+	public void setY(double y) {
 		this.y = y;
 	}
 	public double getZ() {
 		return z;
 	}
-	public void setZ(int z) {
+	public void setZ(double z) {
 		this.z = z;
 	}
 
