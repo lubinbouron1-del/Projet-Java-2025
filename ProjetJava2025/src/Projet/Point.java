e6c06a475469c700a7450d7f151853240c82b647
Lubin <Lubin@192.168.1.12>
2026-01-02 18:50:16 +0100

    refonte classe point

--- ProjetJava2025/src/Projet/Point.java
+++ ProjetJava2025/src/Projet/Point.java
@@ -1,38 +1,34 @@
 package Projet;
 
 public class Point {
-	private double x;
-	private double y;
-	private double z;
+    private double x;
+    private double y;
+    private double z;
 
-	public Point(double x, double y) {
-		this.x = x;
-		this.y = y;
-	}
-	public Point(double x, double y, double z) {
-		this.x = x;
-		this.y = y;
-		this.z = z;
-	}
-	public double getX() {
-		return x;
-	}
-	public void setX(double x) {
-		this.x = x;
-	}
-	public double getY() {
-		return y;
-	}
-	public void setY(double y) {
-		this.y = y;
-	}
-	public double getZ() {
-		return z;
-	}
-	public void setZ(double z) {
-		this.z = z;
-	}
+    // Constructeur 2D (z = 0)
+    public Point(double x, double y) {
+        this(x, y, 0.0);
+    }
 
-	//qlzjvblqe
+    // Constructeur 3D
+    public Point(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    // Getters / Setters
+    public double getX() { return x; }
+    public void setX(double x) { this.x = x; }
+
+    public double getY() { return y; }
+    public void setY(double y) { this.y = y; }
+
+    public double getZ() { return z; }
+    public void setZ(double z) { this.z = z; }
+
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ", " + z + ")";
+    }
 }
-
