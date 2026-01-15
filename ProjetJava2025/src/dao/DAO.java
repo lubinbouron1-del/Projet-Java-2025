ffd503976ebb0d70241cb98275e70197b6d7e4a3
morea <morea@LAPTOP-OOC8UIF5>
2026-01-15 15:55:21 +0100

    truc

---
+++ ProjetJava2025/src/dao/DAO.java
@@ -0,0 +1,470 @@
+package dao;
+
+
+
+
+import java.util.List;
+import java.beans.Statement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import com.mysql.cj.jdbc.MysqlDataSource;
+
+
+
+
+public class DAO {
+	
+	
+	private static String databaseName="corps celestes";
+	private static String url="jdbc:mysql://localhost:3306/"+databaseName+"?serverTimezone=UTC";
+	private static String login="root"; 
+	private static String password="";
+	
+//PLANETE
+	
+	public static void ajouterElement(String nom,String table) {
+		Connection cn=null;
+		java.sql.Statement st = null;
+		ResultSet rs = null;
+		MysqlDataSource mysqlDS = new MysqlDataSource();
+		mysqlDS.setURL(url);
+		mysqlDS.setUser(login);
+		mysqlDS.setPassword(password);
+		try {
+			cn = mysqlDS.getConnection();
+		} 
+		catch (SQLException e1) {
+			System.err.println("Erreur de parcours de connexion");
+			e1.printStackTrace();
+		}
+		try {
+			st = cn.createStatement();
+			String sqlQuery = "INSERT INTO `"+table+"`( `nom`) VALUES ('"+nom+"')";
+			int i = st.executeUpdate(sqlQuery);
+		}
+		catch(SQLException e) {
+			System.err.println("Erreur requete SQL");
+			e.printStackTrace();
+		
+		}
+
+		}
+	
+	public static void suppElement(String nom, String table) {
+		Connection cn=null;
+		java.sql.Statement st = null;
+		ResultSet rs = null;
+		MysqlDataSource mysqlDS = new MysqlDataSource();
+		mysqlDS.setURL(url);
+		mysqlDS.setUser(login);
+		mysqlDS.setPassword(password);
+		try {
+			cn = mysqlDS.getConnection();
+		} 
+		catch (SQLException e1) {
+			System.err.println("Erreur de parcours de connexion");
+			e1.printStackTrace();
+		}
+		try {
+			st = cn.createStatement();
+			String sqlQuery = "DELETE FROM `"+table+"` WHERE nom = '"+nom+"'";
+			int i = st.executeUpdate(sqlQuery);
+		}
+		catch(SQLException e) {
+			System.err.println("Erreur requete SQL");
+			e.printStackTrace();
+		
+		}
+
+		}
+	
+	public static void setMasse(String nom,double masse, String table) {
+		Connection cn=null;
+		java.sql.Statement st = null;
+		ResultSet rs = null;
+		MysqlDataSource mysqlDS = new MysqlDataSource();
+		mysqlDS.setURL(url);
+		mysqlDS.setUser(login);
+		mysqlDS.setPassword(password);
+		try {
+			cn = mysqlDS.getConnection();
+		} 
+		catch (SQLException e1) {
+			System.err.println("Erreur de parcours de connexion");
+			e1.printStackTrace();
+		}
+		try {
+			st = cn.createStatement();
+			String sqlQuery = "UPDATE `"+table+"` SET masse_kg = "+masse+" WHERE nom = '"+nom+"'";
+			int i = st.executeUpdate(sqlQuery);
+		}
+		catch(SQLException e) {
+			System.err.println("Erreur requete SQL");
+			e.printStackTrace();
+		
+		}
+
+		}
+	
+	public static void setRayon(String nom,double rayon, String table) {
+		Connection cn=null;
+		java.sql.Statement st = null;
+		ResultSet rs = null;
+		MysqlDataSource mysqlDS = new MysqlDataSource();
+		mysqlDS.setURL(url);
+		mysqlDS.setUser(login);
+		mysqlDS.setPassword(password);
+		try {
+			cn = mysqlDS.getConnection();
+		} 
+		catch (SQLException e1) {
+			System.err.println("Erreur de parcours de connexion");
+			e1.printStackTrace();
+		}
+		try {
+			st = cn.createStatement();
+			String sqlQuery = "UPDATE `"+table+"` SET rayon_km = "+rayon+" WHERE nom = '"+nom+"'";
+			int i = st.executeUpdate(sqlQuery);
+		}
+		catch(SQLException e) {
+			System.err.println("Erreur requete SQL");
+			e.printStackTrace();
+		
+		}
+		}
+	
+
+	public static void setGravite(String nom,double gravite, String table) {
+		Connection cn=null;
+		java.sql.Statement st = null;
+		ResultSet rs = null;
+		MysqlDataSource mysqlDS = new MysqlDataSource();
+		mysqlDS.setURL(url);
+		mysqlDS.setUser(login);
+		mysqlDS.setPassword(password);
+		try {
+			cn = mysqlDS.getConnection();
+		} 
+		catch (SQLException e1) {
+			System.err.println("Erreur de parcours de connexion");
+			e1.printStackTrace();
+		}
+		try {
+			st = cn.createStatement();
+			String sqlQuery = "UPDATE `"+table+"` SET gravite = "+gravite+" WHERE nom = '"+nom+"'";
+			int i = st.executeUpdate(sqlQuery);
+		}
+		catch(SQLException e) {
+			System.err.println("Erreur requete SQL");
+			e.printStackTrace();
+		
+		}
+		}
+	
+	public static void setTemp(String nom,double temp) {
+		Connection cn=null;
+		java.sql.Statement st = null;
+		ResultSet rs = null;
+		MysqlDataSource mysqlDS = new MysqlDataSource();
+		mysqlDS.setURL(url);
+		mysqlDS.setUser(login);
+		mysqlDS.setPassword(password);
+		try {
+			cn = mysqlDS.getConnection();
+		} 
+		catch (SQLException e1) {
+			System.err.println("Erreur de parcours de connexion");
+			e1.printStackTrace();
+		}
+		try {
+			st = cn.createStatement();
+			String sqlQuery = "UPDATE `planetes` SET temperature = "+temp+" WHERE nom = '"+nom+"'";
+			int i = st.executeUpdate(sqlQuery);
+		}
+		catch(SQLException e) {
+			System.err.println("Erreur requete SQL");
+			e.printStackTrace();
+		
+		}
+		}
+	
+	
+	public static void setTempSur(String nom,double temp) {
+		Connection cn=null;
+		java.sql.Statement st = null;
+		ResultSet rs = null;
+		MysqlDataSource mysqlDS = new MysqlDataSource();
+		mysqlDS.setURL(url);
+		mysqlDS.setUser(login);
+		mysqlDS.setPassword(password);
+		try {
+			cn = mysqlDS.getConnection();
+		} 
+		catch (SQLException e1) {
+			System.err.println("Erreur de parcours de connexion");
+			e1.printStackTrace();
+		}
+		try {
+			st = cn.createStatement();
+			String sqlQuery = "UPDATE `etoile` SET temperature_surface = "+temp+" WHERE nom = '"+nom+"'";
+			int i = st.executeUpdate(sqlQuery);
+		}
+		catch(SQLException e) {
+			System.err.println("Erreur requete SQL");
+			e.printStackTrace();
+		
+		}
+		}
+	
+	public static void addComp_Atm(String nom,String compo) {
+		Connection cn=null;
+		java.sql.Statement st = null;
+		ResultSet rs = null;
+		MysqlDataSource mysqlDS = new MysqlDataSource();
+		mysqlDS.setURL(url);
+		mysqlDS.setUser(login);
+		mysqlDS.setPassword(password);
+		try {
+			cn = mysqlDS.getConnection();
+		} 
+		catch (SQLException e1) {
+			System.err.println("Erreur de parcours de connexion");
+			e1.printStackTrace();
+		}
+		try {
+			st = cn.createStatement();
+			String sqlQuery = "UPDATE planetes SET comp_atm = JSON_ARRAY_APPEND(comp_atm, '$', '"+compo+"') WHERE nom = '"+nom+"'";
+			int i = st.executeUpdate(sqlQuery);
+		}
+		catch(SQLException e) {
+			System.err.println("Erreur requete SQL");
+			e.printStackTrace();
+		
+		}
+		}
+	
+	
+	public static void suppComp_Atm(String nom,String compo) {
+		Connection cn=null;
+		java.sql.Statement st = null;
+		ResultSet rs = null;
+		MysqlDataSource mysqlDS = new MysqlDataSource();
+		mysqlDS.setURL(url);
+		mysqlDS.setUser(login);
+		mysqlDS.setPassword(password);
+		try {
+			cn = mysqlDS.getConnection();
+		} 
+		catch (SQLException e1) {
+			System.err.println("Erreur de parcours de connexion");
+			e1.printStackTrace();
+		}
+		try {
+			st = cn.createStatement();
+			String sqlQuery = "UPDATE planetes SET comp_atm = JSON_REMOVE(comp_atm,JSON_UNQUOTE(JSON_SEARCH(comp_atm, 'one', '"+compo+"')))WHERE nom = '"+nom+"'";
+			int i = st.executeUpdate(sqlQuery);
+		}
+		catch(SQLException e) {
+			System.err.println("Erreur requete SQL");
+			e.printStackTrace();
+		
+		}
+		}
+	
+		
+	public static double getMasse(String nom, String table) {
+		Connection cn=null;
+		java.sql.Statement st = null;
+		ResultSet rs = null;
+		MysqlDataSource mysqlDS = new MysqlDataSource();
+		mysqlDS.setURL(url);
+		mysqlDS.setUser(login);
+		mysqlDS.setPassword(password);
+		try {
+			cn = mysqlDS.getConnection();
+		} 
+		catch (SQLException e1) {
+			System.err.println("Erreur de parcours de connexion");
+			e1.printStackTrace();
+		}
+		try {
+			st = cn.createStatement();
+			String sqlQuery = "SELECT masse_kg FROM "+table+" WHERE nom = '"+nom+"'";
+			rs = st.executeQuery(sqlQuery);
+			if(rs.next())
+			return rs.getDouble("masse_kg");
+		}
+		catch(SQLException e) {
+			System.err.println("Erreur requete SQL");
+			e.printStackTrace();
+		}
+		return -1;
+	}	
+	
+	public static double getRayon(String nom, String table) {
+		Connection cn=null;
+		java.sql.Statement st = null;
+		ResultSet rs = null;
+		MysqlDataSource mysqlDS = new MysqlDataSource();
+		mysqlDS.setURL(url);
+		mysqlDS.setUser(login);
+		mysqlDS.setPassword(password);
+		try {
+			cn = mysqlDS.getConnection();
+		} 
+		catch (SQLException e1) {
+			System.err.println("Erreur de parcours de connexion");
+			e1.printStackTrace();
+		}
+		try {
+			st = cn.createStatement();
+			String sqlQuery = "SELECT rayon_km FROM "+table+" WHERE nom = '"+nom+"'";
+			rs = st.executeQuery(sqlQuery);
+			if(rs.next())
+			return rs.getDouble("rayon_km");
+		}
+		catch(SQLException e) {
+			System.err.println("Erreur requete SQL");
+			e.printStackTrace();
+		}
+		return -1;
+	}	
+		
+	public static double getGravite(String nom) {
+		Connection cn=null;
+		java.sql.Statement st = null;
+		ResultSet rs = null;
+		MysqlDataSource mysqlDS = new MysqlDataSource();
+		mysqlDS.setURL(url);
+		mysqlDS.setUser(login);
+		mysqlDS.setPassword(password);
+		try {
+			cn = mysqlDS.getConnection();
+		} 
+		catch (SQLException e1) {
+			System.err.println("Erreur de parcours de connexion");
+			e1.printStackTrace();
+		}
+		try {
+			st = cn.createStatement();
+			String sqlQuery = "SELECT gravite FROM planetes WHERE nom = '"+nom+"'";
+			rs = st.executeQuery(sqlQuery);
+			if(rs.next())
+			return rs.getDouble("gravite");
+		}
+		catch(SQLException e) {
+			System.err.println("Erreur requete SQL");
+			e.printStackTrace();
+		}
+		return -1;
+	}	
+	
+	public static double getTemp(String nom) {
+		Connection cn=null;
+		java.sql.Statement st = null;
+		ResultSet rs = null;
+		MysqlDataSource mysqlDS = new MysqlDataSource();
+		mysqlDS.setURL(url);
+		mysqlDS.setUser(login);
+		mysqlDS.setPassword(password);
+		try {
+			cn = mysqlDS.getConnection();
+		} 
+		catch (SQLException e1) {
+			System.err.println("Erreur de parcours de connexion");
+			e1.printStackTrace();
+		}
+		try {
+			st = cn.createStatement();
+			String sqlQuery = "SELECT temperature FROM planetes WHERE nom = '"+nom+"'";
+			rs = st.executeQuery(sqlQuery);
+			if(rs.next())
+			return rs.getDouble("temperature");
+		}
+		catch(SQLException e) {
+			System.err.println("Erreur requete SQL");
+			e.printStackTrace();
+		}
+		return -1;
+	}	
+	
+	
+	public static double getTempSur(String nom) {
+		Connection cn=null;
+		java.sql.Statement st = null;
+		ResultSet rs = null;
+		MysqlDataSource mysqlDS = new MysqlDataSource();
+		mysqlDS.setURL(url);
+		mysqlDS.setUser(login);
+		mysqlDS.setPassword(password);
+		try {
+			cn = mysqlDS.getConnection();
+		} 
+		catch (SQLException e1) {
+			System.err.println("Erreur de parcours de connexion");
+			e1.printStackTrace();
+		}
+		try {
+			st = cn.createStatement();
+			String sqlQuery = "SELECT temperature_surface FROM etoile WHERE nom = '"+nom+"'";
+			rs = st.executeQuery(sqlQuery);
+			if(rs.next())
+			return rs.getDouble("temperature_surface");
+		}
+		catch(SQLException e) {
+			System.err.println("Erreur requete SQL");
+			e.printStackTrace();
+		}
+		return -1;
+	}
+		
+	public static String getComp_Atm(String nom) {
+		Connection cn=null;
+		java.sql.Statement st = null;
+		ResultSet rs = null;
+		MysqlDataSource mysqlDS = new MysqlDataSource();
+		mysqlDS.setURL(url);
+		mysqlDS.setUser(login);
+		mysqlDS.setPassword(password);
+		try {
+			cn = mysqlDS.getConnection();
+		} 
+		catch (SQLException e1) {
+			System.err.println("Erreur de parcours de connexion");
+			e1.printStackTrace();
+		}
+		try {
+			st = cn.createStatement();
+			String sqlQuery = "SELECT comp_atm FROM planetes WHERE nom = '"+nom+"'";
+			rs = st.executeQuery(sqlQuery);
+			if(rs.next())
+			return rs.getString("comp_atm");
+		}
+		catch(SQLException e) {
+			System.err.println("Erreur requete SQL");
+			e.printStackTrace();
+		}
+		return "";
+	}	
+
+	
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+
+		double i = getTempSur("Soleil");
+		System.out.println(i);
+		
+	}
+
+}
+
+
+		
+
+
